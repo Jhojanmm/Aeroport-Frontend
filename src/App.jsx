c3211f435072f24b703d63711255fff9aedff3e8
jhojan <jhojanmartinez04@hotmail.com>
2022-10-25 15:46:44 -0500

    Added: condition of max 3 seats for customer

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,70 @@ import { Heading, Box, Image, Flex, Center, Text, Spacer, Tag, Button } from '@c
 
 let count = [];
 
+
 export default function Sillas() {
+
+
+    let seatsAdded = []
     const [reservas, setReservas] = useState([]);
 
+    function add(e, seat) {
+        e.preventDefault();
+        let k = 0;
+        let check = false;
+        if (seatsAdded.length < 3) {
+            for (let i = 0; i < seatsAdded.length; i++) {
+                if (seatsAdded[i] === seat) {
+                    console.log("Eliminado: ", seatsAdded[i])
+                    seatsAdded.splice(i, 1);
+                    check = true;
+                    break
+                }
 
+            }
+
+            if (!check) {
+                seatsAdded.push(seat)
+                console.log("Agregando: ", seat)
+            }
+
+        } else {
+            for (let i = 0; i < seatsAdded.length; i++) {
+                if (seatsAdded[i] === seat) {
+                    console.log("Eliminado: ", seatsAdded[i])
+                    seatsAdded.splice(i, 1);
+                    check = true;
+                    break
+                }
+
+            }
+
+            if (!check) {
+                console.log("Limite maximo de sillas")
+            }
+
+        }
+
+
+        console.log(seatsAdded)
+    }
 
     useEffect(() => {
         Load(setReservas);
     }, [])
 
-    {reservas.map((reservas) => (
-        count.push(reservas.idSilla)
 
-    ))}
+    for (let i = 0; i < 60; i++) {
+        count.push(i)
+
+    }
+
+    // {reservas.map((reservas) => (
+    //   count.push(reservas.idSilla)
+
+    // ))}
+
+
 
     return (
         <>
@@ -31,117 +82,117 @@ export default function Sillas() {
             </Center>
 
 
-            
+
             <Center>
 
                 <div class="row" >
 
-                    <button>{count[0]}</button>
-                    <button>{count[1]}</button>
+                    <button onClick={(e) => add(e, count[0])}>{count[0]}</button>
+                    <button onClick={(e) => add(e, count[1])}>{count[1]}</button>
                     <Button margin={10}></Button>
-                    <button>{count[2]}</button>
-                    <button>{count[3]}</button>
+                    <button onClick={(e) => add(e, count[2])}>{count[2]}</button>
+                    <button onClick={(e) => add(e, count[3])}>{count[3]}</button>
                 </div>
             </Center>
             <Center>
 
                 <div class="row">
 
-                    <button>{count[4]}</button>
-                    <button>{count[5]}</button>
+                    <button onClick={(e) => add(e, count[4])}>{count[4]}</button>
+                    <button onClick={(e) => add(e, count[5])}>{count[5]}</button>
                     <Button margin={10}></Button>
-                    <button>{count[6]}</button>
-                    <button>{count[7]}</button>
+                    <button onClick={(e) => add(e, count[6])}>{count[6]}</button>
+                    <button onClick={(e) => add(e, count[7])}>{count[7]}</button>
                 </div>
             </Center>
             <Center>
                 <div class="row">
 
-                    <button>{count[8]}</button>
-                    <button>{count[9]}</button>
-                    <button>{count[10]}</button>
+                    <button onClick={(e) => add(e, count[8])}>{count[8]}</button>
+                    <button onClick={(e) => add(e, count[9])}>{count[9]}</button>
+                    <button onClick={(e) => add(e, count[10])}>{count[10]}</button>
                     <Button margin={10}></Button>
-                    <button>{count[11]}</button>
-                    <button>{count[12]}</button>
-                    <button>{count[13]}</button>
-                </div>
-
-            </Center>
-            <Center>
-                <div class="row">
-
-                    <button>{count[14]}</button>
-                    <button>{count[15]}</button>
-                    <button>{count[16]}</button>
-                    <Button margin={10}></Button>
-                    <button>{count[17]}</button>
-                    <button>{count[18]}</button>
-                    <button>{count[19]}</button>
+                    <button onClick={(e) => add(e, count[11])}>{count[11]}</button>
+                    <button onClick={(e) => add(e, count[12])}>{count[12]}</button>
+                    <button onClick={(e) => add(e, count[13])}>{count[13]}</button>
                 </div>
 
             </Center>
             <Center>
                 <div class="row">
 
-                    <button>{count[20]}</button>
-                    <button>{count[21]}</button>
-                    <button>{count[22]}</button>
+                    <button onClick={(e) => add(e, count[14])}>{count[14]}</button>
+                    <button onClick={(e) => add(e, count[15])}>{count[15]}</button>
+                    <button onClick={(e) => add(e, count[16])}>{count[16]}</button>
                     <Button margin={10}></Button>
-                    <button>{count[23]}</button>
-                    <button>{count[24]}</button>
-                    <button>{count[25]}</button>
+                    <button onClick={(e) => add(e, count[17])}>{count[17]}</button>
+                    <button onClick={(e) => add(e, count[18])}>{count[18]}</button>
+                    <button onClick={(e) => add(e, count[19])}>{count[19]}</button>
                 </div>
 
             </Center>
             <Center>
                 <div class="row">
 
-                    <button>{count[26]}</button>
-                    <button>{count[27]}</button>
-                    <button>{count[28]}</button>
+                    <button onClick={(e) => add(e, count[20])}>{count[20]}</button>
+                    <button onClick={(e) => add(e, count[21])}>{count[21]}</button>
+                    <button onClick={(e) => add(e, count[22])}>{count[22]}</button>
                     <Button margin={10}></Button>
-                    <button>{count[29]}</button>
-                    <button>{count[30]}</button>
-                    <button>{count[31]}</button>
+                    <button onClick={(e) => add(e, count[23])}>{count[23]}</button>
+                    <button onClick={(e) => add(e, count[24])}>{count[24]}</button>
+                    <button onClick={(e) => add(e, count[25])}>{count[25]}</button>
                 </div>
 
             </Center>
             <Center>
                 <div class="row">
 
-                    <button>{count[32]}</button>
-                    <button>{count[33]}</button>
-                    <button>{count[34]}</button>
+                    <button onClick={(e) => add(e, count[26])}>{count[26]}</button>
+                    <button onClick={(e) => add(e, count[27])}>{count[27]}</button>
+                    <button onClick={(e) => add(e, count[28])}>{count[28]}</button>
                     <Button margin={10}></Button>
-                    <button>{count[35]}</button>
-                    <button>{count[36]}</button>
-                    <button>{count[37]}</button>
+                    <button onClick={(e) => add(e, count[29])}>{count[29]}</button>
+                    <button onClick={(e) => add(e, count[30])}>{count[30]}</button>
+                    <button onClick={(e) => add(e, count[31])}>{count[31]}</button>
                 </div>
 
             </Center>
             <Center>
                 <div class="row">
 
-                    <button>{count[38]}</button>
-                    <button>{count[39]}</button>
-                    <button>{count[40]}</button>
+                    <button onClick={(e) => add(e, count[32])}>{count[32]}</button>
+                    <button onClick={(e) => add(e, count[33])}>{count[33]}</button>
+                    <button onClick={(e) => add(e, count[34])}>{count[34]}</button>
                     <Button margin={10}></Button>
-                    <button>{count[41]}</button>
-                    <button>{count[42]}</button>
-                    <button>{count[43]}</button>
+                    <button onClick={(e) => add(e, count[35])}>{count[35]}</button>
+                    <button onClick={(e) => add(e, count[36])}>{count[36]}</button>
+                    <button onClick={(e) => add(e, count[37])}>{count[37]}</button>
                 </div>
 
             </Center>
             <Center>
                 <div class="row">
 
-                    <button>{count[44]}</button>
-                    <button>{count[45]}</button>
-                    <button>{count[46]}</button>
+                    <button onClick={(e) => add(e, count[38])}>{count[38]}</button>
+                    <button onClick={(e) => add(e, count[39])}>{count[39]}</button>
+                    <button onClick={(e) => add(e, count[40])}>{count[40]}</button>
                     <Button margin={10}></Button>
-                    <button>{count[47]}</button>
-                    <button>{count[48]}</button>
-                    <button>{count[49]}</button>
+                    <button onClick={(e) => add(e, count[41])}>{count[41]}</button>
+                    <button onClick={(e) => add(e, count[42])}>{count[42]}</button>
+                    <button onClick={(e) => add(e, count[43])}>{count[43]}</button>
+                </div>
+
+            </Center>
+            <Center>
+                <div class="row">
+
+                    <button onClick={(e) => add(e, count[44])}>{count[44]}</button>
+                    <button onClick={(e) => add(e, count[45])}>{count[45]}</button>
+                    <button onClick={(e) => add(e, count[46])}>{count[46]}</button>
+                    <Button margin={10}></Button>
+                    <button onClick={(e) => add(e, count[47])}>{count[47]}</button>
+                    <button onClick={(e) => add(e, count[48])}>{count[48]}</button>
+                    <button onClick={(e) => add(e, count[49])}>{count[49]}</button>
                 </div>
 
             </Center>
